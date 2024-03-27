a8479d6d8de4f037c46fee418044fc571caa5bbd
workkashyap <workkashyap@gmail.com>
2024-03-27 11:49:11 +0530

    dropdown changes

--- src/views/pages/operation.vue
+++ src/views/pages/operation.vue
@@ -14,13 +14,16 @@
           </template>
         </Toolbar>
 
+        <!-- Dropdown component -->
+      <Dropdown v-model="selectedUser" :options="users" optionLabel="name" placeholder="Select a User" class="w-full md:w-14rem" />
+
         <DataTable
           :value="filteredUsers"
           v-model:selection="selectedUser"
           dataKey="id"
           :paginator="true"
           :rows="10"
-           size="small"
+          size="small"
           :paginatorTemplate="paginatorTemplate"
           currentPageReportTemplate="Showing {first} to {last} of {totalRecords} users"
           :globalFilterFields="['name', 'username', 'email', 'address.city', 'phone', 'website']"
@@ -89,126 +92,143 @@
     </div>
   </div>
 </template>
-
-<script setup>
+<script>
 import axios from 'axios';
-import { ref, watch } from 'vue';
+import { ref, watch , computed} from 'vue';
 import { useToast } from 'primevue/usetoast';
 
-const toast = useToast();
+export default {
+  setup() {
+    const toast = useToast();
+    
+    const users = ref([]);
+    const filteredUsers = ref([]);
+    const selectedUser = ref(null);
+    const isAddUserDialogVisible = ref(false);
+    const newUser = ref({
+      name: '',
+      username: '',
+      email: '',
+      phone: '',
+      website: '',
+      address: {
+        city: ''
+      }
+    });
+    const globalFilterValue = ref('');
+    
+    const API_URL = 'https://jsonplaceholder.typicode.com/users';
+    
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        users.value = response.data;
+        filteredUsers.value = response.data;
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+    
+    const deleteUser = async () => {
+      if (!selectedUser.value) return;
+    
+      try {
+        await axios.delete(`${API_URL}/${selectedUser.value.id}`);
+        users.value = users.value.filter(user => user.id !== selectedUser.value.id);
+        filteredUsers.value = filteredUsers.value.filter(user => user.id !== selectedUser.value.id);
+        selectedUser.value = null;
+        toast.add({ severity: 'success', summary: 'Successful', detail: 'User Deleted', life: 3000 });
+      } catch (error) {
+        console.error('Error deleting user:', error);
+      }
+    };
+    
+    const openNew = () => {
+      isAddUserDialogVisible.value = true;
+    };
 
-const users = ref([]);
-const filteredUsers = ref([]);
-const selectedUser = ref(null);
-const isAddUserDialogVisible = ref(false);
-const newUser = ref({
-  name: '',
-  username: '',
-  email: '',
-  phone: '',
-  website: '',
-  address: {
-    city: ''
+       
+    
+    const closeAddUserDialog = () => {
+      isAddUserDialogVisible.value = false;
+      // Reset new user data
+      newUser.value = {
+        name: '',
+        username: '',
+        email: '',
+        phone: '',
+        website: '',
+        address: {
+          city: ''
+        }
+      };
+    };
+    
+    const saveNewUser = async () => {
+      try {
+        const response = await axios.post(API_URL, newUser.value);
+        users.value.push(response.data);
+        filteredUsers.value.push(response.data);
+        closeAddUserDialog();
+        toast.add({ severity: 'success', summary: 'Success', detail: 'New user added successfully', life: 3000 });
+      } catch (error) {
+        console.error('Error saving new user:', error);
+        toast.add({ severity: 'error', summary: 'Error', detail: 'Failed to add new user', life: 3000 });
+      }
+    };
+    
+    const confirmDeleteSelected = () => {
+      deleteUser();
+    };
+    
+    fetchUsers();
+    
+    // Watch for changes in global filter value and update filteredUsers accordingly
+    watch(globalFilterValue, (newValue, oldValue) => {
+      filterUsers(newValue);
+    });
+    
+    const filterUsers = (searchTerm) => {
+      if (!searchTerm) {
+        filteredUsers.value = users.value;
+        return;
+      }
+    
+      const lowerCaseSearchTerm = searchTerm.toLowerCase();
+      filteredUsers.value = users.value.filter(user => {
+        return (
+          user.name.toLowerCase().includes(lowerCaseSearchTerm) ||
+          user.username.toLowerCase().includes(lowerCaseSearchTerm) ||
+          user.email.toLowerCase().includes(lowerCaseSearchTerm) ||
+          user.address.city.toLowerCase().includes(lowerCaseSearchTerm) ||
+          user.phone.toLowerCase().includes(lowerCaseSearchTerm) ||
+          user.website.toLowerCase().includes(lowerCaseSearchTerm)
+        );
+      });
+    };
+    
+    const editUser = (user) => {
+      // Set the user details to be edited
+      newUser.value = { ...user };
+      // Open the edit user dialog
+      isAddUserDialogVisible.value = true;
+    };
+    
+    return {
+      users,
+      filteredUsers,
+      selectedUser,
+      isAddUserDialogVisible,
+      newUser,
+      globalFilterValue,
+      openNew,
+      closeAddUserDialog,
+      saveNewUser,
+      confirmDeleteSelected,
+      editUser
+    };
   }
-});
-const globalFilterValue = ref('');
-
-const API_URL = 'https://jsonplaceholder.typicode.com/users';
-
-const fetchUsers = async () => {
-  try {
-    const response = await axios.get(API_URL);
-    users.value = response.data;
-    filteredUsers.value = response.data;
-  } catch (error) {
-    console.error('Error fetching users:', error);
-  }
 };
-
-const deleteUser = async () => {
-  if (!selectedUser.value) return;
-
-  try {
-    await axios.delete(`${API_URL}/${selectedUser.value.id}`);
-    users.value = users.value.filter(user => user.id !== selectedUser.value.id);
-    filteredUsers.value = filteredUsers.value.filter(user => user.id !== selectedUser.value.id);
-    selectedUser.value = null;
-    toast.add({ severity: 'success', summary: 'Successful', detail: 'User Deleted', life: 3000 });
-  } catch (error) {
-    console.error('Error deleting user:', error);
-  }
-};
-
-const openNew = () => {
-  isAddUserDialogVisible.value = true;
-  
-};
-
-const closeAddUserDialog = () => {
-  isAddUserDialogVisible.value = false;
-  // Reset new user data
-  newUser.value = {
-    name: '',
-    username: '',
-    email: '',
-    phone: '',
-    website: '',
-    address: {
-      city: ''
-    }
-  };
-};
-
-const saveNewUser = async () => {
-  try {
-    const response = await axios.post(API_URL, newUser.value);
-    users.value.push(response.data);
-    filteredUsers.value.push(response.data);
-    closeAddUserDialog();
-    toast.add({ severity: 'success', summary: 'Success', detail: 'New user added successfully', life: 3000 });
-  } catch (error) {
-    console.error('Error saving new user:', error);
-    toast.add({ severity: 'error', summary: 'Error', detail: 'Failed to add new user', life: 3000 });
-  }
-};
-
-const confirmDeleteSelected = () => {
-  deleteUser();
-};
-
-fetchUsers();
-
-// Watch for changes in global filter value and update filteredUsers accordingly
-watch(globalFilterValue, (newValue, oldValue) => {
-  filterUsers(newValue);
-});
-
-const filterUsers = (searchTerm) => {
-  if (!searchTerm) {
-    filteredUsers.value = users.value;
-    return;
-  }
-
-  const lowerCaseSearchTerm = searchTerm.toLowerCase();
-  filteredUsers.value = users.value.filter(user => {
-    return (
-      user.name.toLowerCase().includes(lowerCaseSearchTerm) ||
-      user.username.toLowerCase().includes(lowerCaseSearchTerm) ||
-       user.email.toLowerCase().includes(lowerCaseSearchTerm) ||
-      user.address.city.toLowerCase().includes(lowerCaseSearchTerm) ||
-      user.phone.toLowerCase().includes(lowerCaseSearchTerm) ||
-      user.website.toLowerCase().includes(lowerCaseSearchTerm)
-    );
-  });
-};
-
-const editUser = (user) => {
-  // Set the user details to be edited
-  newUser.value = { ...user };
-  // Open the edit user dialog
-  isAddUserDialogVisible.value = true;
-};
-
 </script>
 
 <style scoped>
